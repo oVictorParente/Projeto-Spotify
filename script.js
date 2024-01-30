1928cb04160b1f502e135fb5a0a64366b3a4ca56
Victor Parente <ovictorparente@outlook.com>
2024-01-30 16:23:00 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Foi desenvolvida uma API (no arquivo "artists.json"), a título de teste, para certificar que a aba de busca está funcionando.
+//Ao ser digitado o nome de um artista na parte de busca da página, o JS permite que retorne na tela a foto e o nome do artista digitado.
+
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
